9bf1d4a5102248e22b4d44bd8f3134d75fe2f146
renwish <eugenebasinio@gmail.com>
2022-11-19 15:41:14 +0800

    added other functions related to face encoding, and started typing face classification code

--- main.py
+++ main.py
@@ -21,3 +21,27 @@ def get_encoded_faces():
                 encoded[f.split(".")[0]] = encoding
 
     return encoded
+
+def unknown_image_encoded(img):
+    """
+    encode a face if given the file name
+    """
+    face = fr.load_image_file("faces/" + img)
+    encoding = fr.face_encodings(face)[0]
+
+    return encoding
+
+def classify_face(im):
+    """
+    will look for the faces in a given image and label them if it recognizes the face
+    """
+    faces = get_encoded_faces()
+    faces_encoded = list(faces.values())
+    known_face_names = list(faces.keys())
+
+    img = cv2.imread(im, 1)
+    # img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+    # img = img[:,:,::-1]
+
+    face_locations = face_recognition.face_locations(img)
+    unknown_face_encodings = face_recognition.face_encodings(img, face_locations)
